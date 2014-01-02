6067285321480c21874b3f8bfbab43c60bee7d0b
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2014-01-02 17:42:02 -0500

    Bugfixes with routes.php file

--- app/routes.php
+++ app/routes.php
@@ -41,20 +41,6 @@ Route::group(array(
  */
 App::missing(function($exception)
 {
-	//Throw errors only if
-	$url = Request::path();
-	$pattern = '#^files\/photos\/(.*)-([0-9_]+)x([0-9_]+)?(-[0-9a-z(),\-._]+)*\.(jpg|jpeg|png|gif)$#i';
-	if (!preg_match($pattern, $url, $matches)) {
-    	return View::make('layouts.main')
-    				->nest('content','errors.404');
-    }
+	return View::make('layouts.main')
+    			->nest('content','errors.404');
 });
-
-
-
-/*
- *
- * View Composers
- *
- */
-require app_path().'/composers.php';
